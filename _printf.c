85d5ca67948aab066eb394a05db922f3bfe25c28
Menno007 <Menna.soft3@gmail.com>
2023-10-16 02:27:11 +0300

    editting % -1

--- _printf.c
+++ _printf.c
@@ -7,18 +7,24 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int result = 0, i = 0;
+	int result = 0;
+	int i = 0;
 
 	va_start(ap, format);
-	
 	while (format[i] != '\0')
 	{
-	if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
-		return (-1);
-	if (format[i] == '%')
-		result += print_format(format[i++], ap);
-	else
-		result += write(1, &format[i++], 1);
+		if (format[i] == '%')
+		{
+			if (format[i + 1] != '\0')
+			{
+			result += print_format(format[i + 1], ap);
+			i++;
+			}
+			return (-1);
+		}
+		else
+			result += write(1, &format[i], 1);
+		++i;
 	}
 	return (result);
 }
--- main.c
+++ main.c
@@ -10,46 +10,10 @@
 int main(void)
 {
 	int x;
-/*	x = printf("%");
+	x = printf("%");
 	printf("%d\n" ,x);
 	x = _printf("%");
 	printf("%d\n" ,x);
 
-	x = printf("%!\n");
-	printf("%d\n" ,x);
-	x = _printf("%!\n");
-	printf("%d\n" ,x);
-
-	x = printf("%K\n");
-	printf("%d\n" ,x);
-	x = _printf("%K\n");
-	printf("%d\n" ,x);
-
-	x = printf("%c",'\0');
-	printf("%d\n" ,x);
-	x = _printf("%c",'\0');
-	printf("%d\n" ,x);
-
-	x = printf(NULL);
-	printf("%d\n" ,x);
-	x =_printf(NULL);
-	printf("%d\n" ,x);
-**/
-_printf("%");
-printf("\n");
-
-//printf("%r");
-printf("\n");
-x = _printf("%r");
-printf("%d\n", x);
-printf("\n");
-
-// _printf("%%");
-// _printf("\n");
-// printf("%%");
-// printf("\n");
-// _printf("%");
-// printf("\n");
-// printf("%");
 	return (0);
 }
