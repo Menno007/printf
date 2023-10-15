70cd78dcb1a302672d9677026db3a606baf44018
Menno007 <Menna.soft3@gmail.com>
2023-10-15 22:53:43 +0300

    try

--- main.c
+++ main.c
@@ -7,32 +7,3 @@
  *
  * Return: Always 0
  */
-int main(void)
-{
-	int result;
-
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	_printf("Percent:[%%]\n");
-	printf("Percent:[%%]\n");
-	result = _printf("Character:[%c]\n", 'H');
-	printf("%d\n", result);
-	result = printf("Character:[%c]\n", 'H');
-	printf("%d\n", result);
-	result = _printf("String:[%s]\n", NULL);
-	printf("%d\n", result);
-	result = printf("String:[%s]\n", NULL);
-	printf("%d\n", result);
-	result = _printf("Percent:[%]\n");
-	printf("%d\n", result);
-	result = printf("Percent:[%]\n");
-	printf("%d\n", result);
-	result = printf(NULL);
-	printf("%d\n", result);
-	result = _printf(NULL);
-	printf("%d\n", result);
-
-	return (0);
-}
