823e0be04db280dffffc22fe3b201547f569d873
Menno007 <Menna.soft3@gmail.com>
2023-10-16 03:48:31 +0300

    change if condition for _printif

--- _printf.c
+++ _printf.c
@@ -7,16 +7,19 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int result = 0;
+	int result = 0, i = 0;
 
 	va_start(ap, format);
 	while (*format != '\0')
 	{
+		if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
+			return (-1);
 		if (*format == '%')
 			result += print_format(*(++format), ap);
 		else
 			result += write(1, format, 1);
 		++format;
+		++i;
 	}
 	return (result);
 }
