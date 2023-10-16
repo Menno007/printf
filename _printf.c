91aa9c656c96350b98d3b8f92fa282279e86cb54
Menno007 <Menna.soft3@gmail.com>
2023-10-16 03:40:34 +0300

    chang from format[i] to *format

--- _printf.c
+++ _printf.c
@@ -8,24 +8,15 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 	int result = 0;
-	int i = 0;
 
 	va_start(ap, format);
-	while (format[i] != '\0')
+	while (*format != '\0')
 	{
-		if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
-			return (-1);
-		if (format[i] == '%')
-		{
-			if (format[i + 1] != '\0')
-			{
-			result += print_format(format[i + 1], ap);
-			i++;
-			}
-		}
+		if (*format == '%')
+			result += print_format(*(++format), ap);
 		else
-			result += write(1, &format[i], 1);
-		++i;
+			result += write(1, format, 1);
+		++format;
 	}
 	return (result);
 }
