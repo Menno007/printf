9977c495117f2c24ada9a819583acc7144481109
Menno007 <Menna.soft3@gmail.com>
2023-10-16 18:14:02 +0300

    w

--- _printf.c
+++ _printf.c
@@ -22,12 +22,12 @@ int _printf(const char *format, ...)
 			if (format[i + 1] != '\0')
 			{
 			result += print_format(format[i + 1], ap);
-			i++;
+			++i;
 			}
 		}
 		else
 			result += write(1, &format[i], 1);
-		i++;
+		++i;
 	}
 	return (result);
 }
