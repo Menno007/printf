b99dae381cf89cb5839fd6919e3b7c5cbcf45024
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-10-17 00:43:10 +0300

    test

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+#define BUFF_SIZE 1024
+
+int _printf(const char *format, ...);
+int print_char(char c);
+int print_string(const char *str);
+int print_percent(void);
+int print_format(char format, va_list args);
+int print_int(int integer);
+
+#endif
